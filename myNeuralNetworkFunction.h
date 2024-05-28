18070cf504ec4e9f74b6aae46833640faae85bcf
roxi <roxana.podean@student.upt.ro>
2024-05-29 02:35:16 +0300

    added headers files for Neural Network

---
+++ myNeuralNetworkFunction.h
@@ -0,0 +1,32 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * myNeuralNetworkFunction.h
+ *
+ * Code generation for function 'myNeuralNetworkFunction'
+ *
+ */
+
+#ifndef MYNEURALNETWORKFUNCTION_H
+#define MYNEURALNETWORKFUNCTION_H
+
+/* Include files */
+#include "rtwtypes.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Function Declarations */
+extern double myNeuralNetworkFunction(const double x1[6]);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+/* End of code generation (myNeuralNetworkFunction.h) */
